78d14320f3d6a31ec4c562a962b5c3ab2c8fc95e
Maryam Mirzada <marimmirzada@gmail.com>
2026-02-01 19:29:45 +0430

    fetch and cleanUp data

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,53 @@ import SearchBar from "./components/SearchBar";
 const API_key = '89bdf090'
 function App(){
   const [searchText, setSearchText] = useState('') ; 
-  const Trim = setSearchText.trim()
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [Movies, setMovies] = useState([]);
+
+  const Trim = setSearchText.trim();
 
   useEffect(() => {
-if(!Trim) {
-  return;
-}if(!API_key){
-  return;
-}
-const controll = new AbortController(); 
-async function fetchMovie(){
-try{
-  const FetchData = fetch(`https://www.omdbapi.com/apikey=${API_key}&s=${searchText}`);
- }catch(err){
+    if (!Trim) {
+      setError("");
+      setLoading(false);
+      setMovies([]);
+      return;
+    }
+    if (!API_key) {
+      setError("missing ADPI API");
+      setLoading(false);
+      setMovies([]);
+      return;
+    }
+    const controll = new AbortController();
+    async function fetchMovie() {
+      try {
+        setLoading(true);
+        setError("");
+        const FetchData =  await fetch(
+          `https://www.omdbapi.com/apikey=${API_key}&s=${searchText}`{
+            signal : controll.signal
+          }
+          
+        );
+        const data = await FetchData.json()
+        if(data.Response === 'false'){
+          setMovies([])
+          setError(data.error) || 'No results'
+        }
+        setMovies(data.search || [])
+      } catch (err) {
+        setError("Fail to fetch movie");
+      } finally {
+        setLoading(false);
+      }
+    }
+      fetchMovie() ;
+      return () => controll.abort()  }, [Trim]);
 
-}finally{
 
-}
-}
-  } , [Trim])
+
   return (
     <div>
       <SearchBar></SearchBar>
